f9d742fb6820c1c57d18eccbec8a4268e7028749
eattonton <eattonton@qq.com>
2022-04-10 11:04:26 +0800

    04-10 C

--- shanghaifabu.py
+++ shanghaifabu.py
@@ -53,22 +53,47 @@ def _readJson(fNa, fileCode='utf-8'):
 
 def GetSpanLines(url):
     arr1 = []
-    content = __getWebHtml(url)
-    soup = BeautifulSoup(content, 'lxml')  # 使用lxml解析器
-    divContent = soup.findAll('div',id='js_content')
-    pItems = divContent[0].findAll('p')
-    for pItem in pItems:
-        spanItem = pItem.findAll('span', style="font-size: 16px;")
-        if spanItem:
-            if spanItem and spanItem[0].string:
-                arr1.append(spanItem[0].string)
-            elif spanItem[0].contents and len(spanItem[0].contents) > 0:
-                str1=""
-                for sp1 in spanItem[0].contents:
-                    if sp1.string:
-                        str1 = str1 + sp1.string
-                if str1:
-                    arr1.append(str1)
+    if url:
+        content = __getWebHtml(url)
+        soup = BeautifulSoup(content, 'lxml')  # 使用lxml解析器
+        divContent = soup.findAll('div',id='js_content')
+        pItems = divContent[0].findAll('p')
+        for pItem in pItems:
+            #spanItem = pItem.findAll('span',{'style':'font-size: 16px;'})
+            spanItems = pItem.findAll('span')
+            if spanItems:
+                for spanItem in spanItems:
+                    if 'font-size: 16px;' in str(spanItem) and 'data-src' not in str(spanItem):
+                        if spanItem and spanItem.string:
+                            arr1.append(spanItem.string)
+                        elif spanItem.contents and len(spanItem.contents) > 0:
+                            str1=""
+                            for sp1 in spanItem.contents:
+                                if sp1.string:
+                                    str1 = str1 + sp1.string
+                            if str1:
+                                arr1.append(str1)
+    return arr1
+
+def GetSpanLines2(url):
+    arr1 = []
+    if url:
+        content = __getWebHtml(url)
+        soup = BeautifulSoup(content, 'lxml')  # 使用lxml解析器
+        divContent = soup.findAll('div',id='js_content')
+        pItems = divContent[0].findAll('p')
+        for pItem in pItems:
+            spanItem = pItem.findAll('span')
+            if spanItem:
+                if spanItem and spanItem[0].string:
+                    arr1.append(spanItem[0].string)
+                elif spanItem[0].contents and len(spanItem[0].contents) > 0:
+                    str1=""
+                    for sp1 in spanItem[0].contents:
+                        if sp1.string:
+                            str1 = str1 + sp1.string
+                    if str1:
+                        arr1.append(str1)
     return arr1
 
 #读取上海新冠数据
@@ -126,7 +151,16 @@ def GetSHCOVIDJSON(urls,file2):
         objResult["history"]=history
         _write(file2, json.dumps(objResult, sort_keys=False, ensure_ascii=False, separators=(',', ':')))
     return file2
-    
+
+#根据各个发布平台的数据读取
+def GetSHCOVIDJSONSByZone(urls2,file2):
+    for oUrl2 in urls2:
+        strDate0 = oUrl2["d"]
+        for oUrl in oUrl2["urls"]:
+            zone1 = oUrl["zone"]
+            arr2 = GetSpanLines2(oUrl["url"])
+            print(zone1,arr2)
+
 def ZipJSON(file1):
     _name = os.path.join(os.path.basename(file1))
     _name2 = _name.split('.')[0]+".zip"
@@ -155,5 +189,28 @@ if __name__ == "__main__":
     urls.append({"d":"2022-04-06","url":"https://mp.weixin.qq.com/s/8bljTUplPh1q4MXb6wd_gg"})
     urls.append({"d":"2022-04-07","url":"https://mp.weixin.qq.com/s/HTM47mUp0GF-tWXkPeZJlg"})
     urls.append({"d":"2022-04-08","url":"https://mp.weixin.qq.com/s/79NsKhMHbg09Y0xaybTXjA"})
+    urls.append({"d":"2022-04-09","url":"https://mp.weixin.qq.com/s/_Je5_5_HqBcs5chvH5SFfA"})
     file1 = GetSHCOVIDJSON(urls, "./sh.json")
     ZipJSON(file1)
+    #arr1 = GetSpanLines("https://mp.weixin.qq.com/s/_Je5_5_HqBcs5chvH5SFfA")
+    #print(arr1)
+    #2022-04-09
+    urls2 = []
+    urls2.append({"d":"2022-04-09","urls":[{"zone":"浦东新区","url":"https://mp.weixin.qq.com/s/RpjLh0WT7KONsniM1dMYZQ"}
+                                        ,{"zone":"黄浦区","url":"https://mp.weixin.qq.com/s/mN33_Vz-KfkgF84htTQ8dw"}
+                                        ,{"zone":"静安区","url":"https://mp.weixin.qq.com/s/1utEBCXRSfRByj9N26e_5Q"}
+                                        ,{"zone":"徐汇区","url":"https://mp.weixin.qq.com/s/6rjFiePzmWK2Qukj0iuGZA"}
+                                        ,{"zone":"长宁区","url":"https://mp.weixin.qq.com/s/DH9QDy6hBM93inLIfgHh4g"}
+                                        ,{"zone":"普陀区","url":"https://mp.weixin.qq.com/s/pao19i6t--4NJLP4nDGFYg"}
+                                        ,{"zone":"虹口区","url":"https://mp.weixin.qq.com/s/_3ClkLBV-BihWOdhnis23Q"}
+                                        ,{"zone":"杨浦区","url":"https://mp.weixin.qq.com/s/6_Jjsr0JZfbBP4MTVCwmug"}
+                                        ,{"zone":"宝山区","url":"https://mp.weixin.qq.com/s/NUBmVXRQEZUwCngi8nIZjQ"}
+                                        ,{"zone":"闵行区","url":"https://mp.weixin.qq.com/s/skg1jQ4Yq4o16e1FnXPrug"}
+                                        ,{"zone":"嘉定区","url":"https://mp.weixin.qq.com/s/bvs5rvv4fKaY3p4VPeqGIQ"}
+                                        ,{"zone":"金山区","url":""}
+                                        ,{"zone":"松江区","url":"https://mp.weixin.qq.com/s/w4sBIrT9jYpvNd4QwpHLYQ"}
+                                        ,{"zone":"青浦区","url":"https://mp.weixin.qq.com/s/WYLeKd6sWJ6cd6a7a6nGqA"}
+                                        ,{"zone":"奉贤区","url":"https://mp.weixin.qq.com/s/tWB5aXSayxWh763kPiSShw"}
+                                        ,{"zone":"崇明区","url":"https://mp.weixin.qq.com/s/OtMHyuC9xT0pKj5m0yhiIA"}]
+                })
+    #GetSHCOVIDJSONSByZone(urls2, "")
